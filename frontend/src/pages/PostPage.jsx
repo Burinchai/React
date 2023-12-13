3ac133ede3387f5964cc893c12ed980a49d1b12e
maji1106 <nnnpphotmail.com@gmail.com>
2023-12-13 15:40:31 +0700

    first commit

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const PostPage = () => {
   return (
-    <div>PostPage</div>
+    <div className="post-page">
+        <h1>
+            {""}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad animi accusantium quos placeat quae exercitationem tenetur ducimus, autem nesciunt fugiat eum quo perspiciatis, saepe unde aut earum voluptates et odio.
+        </h1>
+        <time> 12 DEC 2023 - 18:00</time>
+    </div>
   )
 }
 
